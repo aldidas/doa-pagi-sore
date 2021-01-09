78e3500ecc3ae8a7313c2fa732a0d41cf38a8e2d
aldidas <aldi@daswanto.com>
2021-01-09 05:10:43 -0500

    counter UI updated

--- src/components/counter.js
+++ src/components/counter.js
@@ -10,34 +10,32 @@ const Counter = () => {
     }
   }
   return (
-    <div className="bg-primary rounded-lg mr-2 mb-2 shadow-md text-center p-4 fixed right-0 bottom-0">
+    <div className="bg-primary fixed inset-x-0 bottom-0 flex items-center p-2 space-x-2">
       <h5 className="mb-1">Counter</h5>
-      <div className="flex justify-center space-x-2 items-center">
-        <button
-          className="flex justify-center items-center shadow-lg rounded-full w-12 h-12 block bg-white"
-          onClick={add}
+      <button
+        className="flex h-10 justify-center items-center shadow-lg rounded-lg flex-grow bg-secondary"
+        onClick={add}
+      >
+        <span className="block text-xl">{count}</span>
+      </button>
+      <button
+        className="block rounded-lg w-10 shadow-lg bg-red-600"
+        onClick={reset}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="#FFFFFF"
         >
-          <span className="block text-xl text-black">{count}</span>
-        </button>
-        <button
-          className="right-0 top-0 block rounded-full w-8 h-8 bg-red-600"
-          onClick={reset}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="#FFFFFF"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
-      </div>
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </button>
     </div>
   )
 }
